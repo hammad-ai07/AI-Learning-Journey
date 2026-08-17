689fd0e8ccead3e0498194faa7aba3868fe01592
Muhammad Hammad Iqbal <ranahammad4413@gmail.com>
2026-08-17 22:49:10 +0500

    Day 1: Python Lesson 1 practice

---
+++ python/lesson_1/practice.py
@@ -0,0 +1,16 @@
+name = "Hammad"
+age = 19
+marks = 90
+
+print(name)
+print(age)
+print(marks)
+
+
+name = "Hammad"
+university = "University of Sargodha"
+degree = "BS Artificial Intelligence"
+
+print(name)
+print(university)
+print(degree)
